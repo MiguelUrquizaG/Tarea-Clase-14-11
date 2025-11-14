ce79f7c38f1a5067a3acbc6b0e92b323eaab02ae
Alumno <urquiza.gamig24@triana.salesianos.edu>
2025-11-14 09:11:44 +0100

    Primer commit

---
+++ tareaclase-14-11/src/main/java/com/salesianostriana/dam/tareaclase_14_11/Tareaclase1411Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.tareaclase_14_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tareaclase1411Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tareaclase1411Application.class, args);
+	}
+
+}
