8f1818513fcac516494d3899b1de4df221459d61
Alumno <urquiza.gamig24@triana.salesianos.edu>
2025-11-19 10:07:28 +0100

    Clase

--- tareaclase-14-11/src/main/java/com/salesianostriana/dam/tareaclase_14_11/controller/MonumentoController.java
+++ tareaclase-14-11/src/main/java/com/salesianostriana/dam/tareaclase_14_11/controller/MonumentoController.java
@@ -3,6 +3,8 @@ package com.salesianostriana.dam.tareaclase_14_11.controller;
 import com.salesianostriana.dam.tareaclase_14_11.dto.EditMonumentoCommand;
 import com.salesianostriana.dam.tareaclase_14_11.model.Monumento;
 import com.salesianostriana.dam.tareaclase_14_11.services.MonumentoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +15,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/monumento")
 @RequiredArgsConstructor
+@Tag(name = "Monumento Controller" ,description = "Es el controller que tiene los métodos de monumentos.")
 public class MonumentoController {
 
     public final MonumentoService monumentoService;
 
+    @Operation(description = "Este método se dedica a traer todos los monumentos")
     @GetMapping
     public List<Monumento> getAll(){
         return monumentoService.findAll();
