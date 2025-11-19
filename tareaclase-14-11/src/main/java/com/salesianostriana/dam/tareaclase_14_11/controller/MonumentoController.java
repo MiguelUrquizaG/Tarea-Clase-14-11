8c2788da25c1dfa71042bd7c9005d7d47f54fcba
Alumno <urquiza.gamig24@triana.salesianos.edu>
2025-11-19 13:27:58 +0100

    Ejemplo Clase

--- tareaclase-14-11/src/main/java/com/salesianostriana/dam/tareaclase_14_11/controller/MonumentoController.java
+++ tareaclase-14-11/src/main/java/com/salesianostriana/dam/tareaclase_14_11/controller/MonumentoController.java
@@ -4,6 +4,11 @@ import com.salesianostriana.dam.tareaclase_14_11.dto.EditMonumentoCommand;
 import com.salesianostriana.dam.tareaclase_14_11.model.Monumento;
 import com.salesianostriana.dam.tareaclase_14_11.services.MonumentoService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,13 +25,35 @@ public class MonumentoController {
 
     public final MonumentoService monumentoService;
 
-    @Operation(description = "Este método se dedica a traer todos los monumentos")
+
     @GetMapping
+    @Operation(description = "Este método se dedica a traer todos los monumentos")
+    @ApiResponses({
+
+    })
     public List<Monumento> getAll(){
         return monumentoService.findAll();
     }
 
+
     @GetMapping("/{id}")
+    @Operation(summary = "Obtiene el monumento que tu le indiques según el id.",description = "Devuelve un monumento.")
+    @ApiResponses( {
+            @ApiResponse(responseCode = "200",description = "Monumento encontrado correctamente"),
+            @ApiResponse(responseCode = "404",description = "Monumento no encontado error" ,content =  @Content
+                    ( mediaType = "application/json",schema = @Schema(implementation = Monumento.class),examples = @ExampleObject(value = """
+            
+                    {
+                      "type": "about:blank",
+                      "title": "Monumento no encontrado.",
+                      "status": 404,
+                      "detail": "No hay un monumento con ese ID: 5",
+                      "instance": "/monumento/5"
+                    }
+            
+            """)))
+    })
+
     public Monumento getById(@PathVariable Long id){
         return monumentoService.findById(id);
     }
--- tareaclase-14-11/src/main/java/com/salesianostriana/dam/tareaclase_14_11/dto/EditMonumentoCommand.java
+++ tareaclase-14-11/src/main/java/com/salesianostriana/dam/tareaclase_14_11/dto/EditMonumentoCommand.java
@@ -1,14 +1,16 @@
 package com.salesianostriana.dam.tareaclase_14_11.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record EditMonumentoCommand(
 
-     String codigoPais,
-     String nombrePais,
-     String nombreCiudad,
-     double latitud,
-     double longitud,
-     String nombreMonumento,
-     String descripcion,
-     String urlFoto
+     @Schema(description = "Código de país",examples = "EN") String codigoPais,
+     @Schema(description = "Nombre del país", examples = "Inglaterra") String nombrePais,
+     @Schema(description = "Nombre de la ciudad",examples = "Londres") String nombreCiudad,
+     @Schema(description = "Latitud en la que se encuentra el monumento.",examples = "1231º") double latitud,
+     @Schema(description = "Longitud en la que se encuentra el monumento.",examples = "4123") double longitud,
+     @Schema(description = "Nombre del monumento",examples = "London Eye") String nombreMonumento,
+     @Schema(description = "Descripción del monumento.",examples = "Es un monumento muy curioso y bonito.") String descripcion,
+     @Schema(description = "Url de la imagen de la foto",examples = "www.wikipedia.org/imagen") String urlFoto
 ) {
 }
